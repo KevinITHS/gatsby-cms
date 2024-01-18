e7d87524238976992c98cf3152d9997ca0ae48dc
KevinITHS <kevin.ali@iths.se>
2024-01-18 15:10:37 +0100

    updated css

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,7 @@ import Layout from "../components/layout";
 export default function NotFoundPage() {
   return (
     <Layout>
-      <div className=" w-full flex-col flex item-center justify-center">
+      <div className="w-full flex-col flex item-center justify-center">
         <h2>404 Not Found</h2>
         <h3>The requested URL was not found on this server.</h3>
         <p>Click on the button to go back to homepage</p>
